8ea05cffd09ecdeef078da73a232df61f7b8bc89
Giovanni Camargo <Giovanni.Camargo@ab-inbev.com>
2024-09-16 08:48:53 -0300

    closes: #2

--- courier/resource.py
+++ courier/resource.py
@@ -8,6 +8,7 @@ service = CourierService()
 class CourierList(Resource):
     parser = reqparse.RequestParser(bundle_errors=True)
     parser.add_argument("name", required=True, type=str, help="Name is required")
+    parser.add_argument("last_name", required=True, type=str, help="Name is required")
     parser.add_argument("email", required=True, type=str, help="Email is required")
     parser.add_argument(
         "document", required=True, type=str, help="Document is required"
@@ -18,7 +19,7 @@ class CourierList(Resource):
 
     def post(self):
         args = self.parser.parse_args(strict=True)
-        return service.save(**args).to_json(), 201
+        return service.save(**args), 201
 
     def get(self):
         return service.get_all()
