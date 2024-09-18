3db43e8d9c3a9c8ab4a3f3e8b803fea18ae79425
Giovanni Camargo <Giovanni.Camargo@ab-inbev.com>
2024-09-18 15:40:03 -0300

    chore: pip freeze

--- commons/send_email.py
+++ commons/send_email.py
@@ -1,10 +1,11 @@
+import os
 import requests
 
 
 def send_email(text: str, subject: str):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox407d1d6bfcf044eeaccccf81ada967fa.mailgun.org/messages",
-        auth=("api", "280e81e2fd149cbe53e09fd0b12900ab-7a3af442-6bb94cce"),
+        auth=("api", os.environ.get("MAILGUN_API_KEY")),
         data={
             "from": "Excited User <mailgun@sandbox407d1d6bfcf044eeaccccf81ada967fa.mailgun.org>",
             "to": ["giovanni.bayerlein.dev@gmail.com"],
